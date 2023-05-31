60489d88e3b1fb1f76c4bbae34afd366286ac624
Pineapple217 <rosierjules@gmail.com>
2023-05-31 10:22:06 +0200

    create db

--- src/repository.py
+++ src/repository.py
@@ -3,6 +3,7 @@ import logging
 import os
 from peewee import *
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()
 
@@ -12,12 +13,19 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
 # TIMEZONE = os.getenv("TIMEZONE")
+conn = psycopg2.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
 
 logger = logging.getLogger(__name__)
 
 db = PostgresqlDatabase(
     host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_DATABASE
 )
+conn.autocommit = True
+cursor = conn.cursor()
+sql = f"CREATE database [IF NOT EXISTS] {DB_DATABASE}"
+cursor.execute(sql)
+print("Database has been created successfully !!")
+conn.close()
 
 
 class BaseModel(Model):
