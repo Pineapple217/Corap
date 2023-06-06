fbc0f6e36d5fb90eafc3d5a5c7649974056573a2
Pineapple217 <rosierjules@gmail.com>
2023-06-06 20:38:05 +0200

    defect detect

---
+++ src/defect_detect.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+import logging
+import numpy as np
+
+import pandas as pd
+
+from repository import Scrape
+
+logger = logging.getLogger(__name__)
+
+
+def defect_detect():
+    logger.info("Defect detect...")
+
+    delta_datetime = datetime.now() - timedelta(hours=24)
+    rows = Scrape.select().where(Scrape.time_scraped > delta_datetime).dicts()
+    scrapes = pd.DataFrame(rows).set_index(["deveui", "id"])
+
+    defects = []
+    for dev_id, dev_df in scrapes.groupby(level=0):
+        logger.info(f"Checking {dev_id}")
+        if not np.all(dev_df["temp"] == dev_df["temp"].iloc[0]):
+            continue
+        if not np.all(dev_df["co2"] == dev_df["co2"].iloc[0]):
+            continue
+        if not np.all(dev_df["humidity"] == dev_df["humidity"].iloc[0]):
+            continue
+        defects.append(dev_id)
+    logger.info(f"Defective devices (count: {len(defects)}): {defects}")
